88e20bad665d488d6cd5f1dfe278918a7068ff2e
Akshay Gupta <akshaygupta@Akshays-MacBook-Pro.local>
2022-12-03 15:57:44 +0530

    Initial Commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/aksgupta98/Hotel-Booking-System/pkg/config"
+	"github.com/aksgupta98/Hotel-Booking-System/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
